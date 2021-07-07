e8c8fcfb1077815d5d0bb469feb65638ba1e85b5
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-07-07 17:11:34 -0300

    Add files via upload

---
+++ lucas_210707.py
@@ -0,0 +1,43 @@
+print("question 2")
+for e in range(1,11):
+    print(e)
+print("question 3")
+for e in range(1,11):
+    if e > 5:
+        print(e)
+print("question 4")
+for e in range(1,11):
+    if e <= 3:
+        print(e)
+print("question 5")
+for e in range(1,11):
+    if e <= 4:
+        print(e)
+    if e > 8:
+        print(e)
+print("question 6")
+for e in range(1,11):
+    if e > 3 and e < 6:
+        print(e)
+print("question 7")
+ow=[2,4,6,8,10,12]
+print("question 8")
+for rl in range(1,11):
+        if rl*2 < 13:
+            print(rl*2)
+print("question 9")
+bl=False
+print("question 10")
+for mnt in range(1,11):
+    print(mnt)
+    if mnt > 8:
+        bl=True
+    if bl == True:
+        print("There was at least one number bigger than 8")
+    else:
+        print("There were no numbers bigger than 8")
+
+
+
+
+
